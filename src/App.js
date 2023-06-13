4396fd634f3902b90bd2fa2f9914222ad8579d3b
FujiHai <liufushihai@gmail.com>
2023-06-13 22:30:19 +0800

    chore: init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { ButtonPage } from "./button/ButtonPage";
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonPage />
+    </div>
+  );
+}
+
+export default App;
